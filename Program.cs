ca7be49b9fdd6cc911bd9a02acec3f5f743ded12
milanesiluca <luca.milanesi@live.se>
2024-05-20 13:34:37 +0200

    removed wl

--- Program.cs
+++ Program.cs
@@ -7,5 +7,4 @@ var amonth = Utilities.CalculateYearlyWage(amount, months);
 var amountWithBonus = Utilities.CalculateYearlyWage(amount, months, 1000);
 Console.WriteLine($"Yearly Wage: {amonth}");
 Console.WriteLine($"Yearly Wage with Bonus: {amountWithBonus}");
-Console.WriteLine("cIAO");
 
