b594465646282e8bc9e25740d327ea6a691b0bcf
milanesiluca <luca.milanesi@live.se>
2024-05-20 15:08:43 +0200

    New Projecrt

---
+++ PersonellPayrollSystem/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonellPayrollSystem
+{
+    internal class Employee
+    {
+        private string name = string.Empty;
+        private string salary = string.Empty;
+        public Employee(string nameE, string salaryE) {
+
+            name = nameE;
+            salary = salaryE;
+        
+        }
+
+        public void getEmployeeDetails(Employee emp)
+        {
+            if (name == string.Empty)
+            {
+                Console.WriteLine("Sorry, no employee in the register");
+                return;
+            }
+
+            Console.WriteLine("Employee's name: " + emp.name);
+            Console.WriteLine("Employee's salary: " + emp.salary);
+        }
+    }
+}
---
+++ PersonellPayrollSystem/Register.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonellPayrollSystem
+{
+    internal class Register
+    {
+
+        private Employee[] employeesList;
+
+        public void addEmplyee()
+        {
+            Console.Write("Insert the emplyee's name: ");
+            string nameP = Console.ReadLine();
+
+            Console.Write("Insert the Salary in USD: ");
+            string salaryP = Console.ReadLine();
+
+            Employee newEmp = new Employee(nameP, salaryP);
+            employeesList.Append(newEmp);
+        }
+
+        public void printEmplyeeList() {
+
+            foreach (Employee employee in employeesList)
+            {
+                employee.getEmployeeDetails(employee);
+            }
+        
+        }
+
+    }
+}
