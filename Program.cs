ec173830d16a54f83cccb2847db3339e7ae63484
milanesiluca <luca.milanesi@live.se>
2024-05-20 13:14:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using BethanysPiesShopHRM;
+
+int amount = 1234;
+int months = 12;
+
+var amonth = Utilities.CalculateYearlyWage(amount, months);
+var amountWithBonus = Utilities.CalculateYearlyWage(amount, months, 1000);
+Console.WriteLine($"Yearly Wage: {amonth}");
+Console.WriteLine($"Yearly Wage with Bonus: {amountWithBonus}");
+
