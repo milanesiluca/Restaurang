e070a87401de8f45b210fb8348e6b8f11089154a
milanesiluca <luca.milanesi@live.se>
2024-05-20 13:21:17 +0200

    NEW ROW

--- Program.cs
+++ Program.cs
@@ -7,4 +7,5 @@ var amonth = Utilities.CalculateYearlyWage(amount, months);
 var amountWithBonus = Utilities.CalculateYearlyWage(amount, months, 1000);
 Console.WriteLine($"Yearly Wage: {amonth}");
 Console.WriteLine($"Yearly Wage with Bonus: {amountWithBonus}");
+Console.WriteLine("cIAO");
 
